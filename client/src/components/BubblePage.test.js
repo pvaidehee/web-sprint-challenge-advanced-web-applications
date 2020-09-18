12043eabef6a8ee6f2a28acb6e3e8214020d4903
vaidehee patel <pvaidehee@gmail.com>
2020-09-18 23:05:32 +0530

    test tries

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
-import BubblePage from "./BubblePage";
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import BubblePage from './BubblePage'
 import apiAuth from 'axios'
 
 jest.mock('../utils/auth')
@@ -11,13 +11,13 @@ const colors = [
     code: {
       hex: '#f0f8ff',
     },
-    id:1,
+    id: 1,
     favorite: false,
   },
 ]
 
-test("Fetches data and renders the bubbles", async () => {
+test('Fetches data and renders the bubbles', async () => {
   apiAuth.get.mockResolvedValueOnce({data: colors})
   render(<BubblePage />)
   expect(getByTestId('loading')).toHaveTextContent('loading')
-});
+})
